afc7bbc26bd6d5a0180b182d229bf9e5c1c5b5bc
Oleg <pololeg38@gmail.com>
2022-07-25 17:49:32 +0300

    first commit

---
+++ code.js
@@ -0,0 +1,13 @@
+let userName = 'Alex';
+let answer = confirm("Привет, тебе есть 18?");
+if (answer == true) { console.log(`Welcome, ${userName}`);
+} else {
+    console.log("By!");
+}
+let userAnswer = prompt("Are u Alex?");
+if (userAnswer == "Yes") {
+    console.log("Hi");
+} else {
+    console.log("No permission");
+}
+   
